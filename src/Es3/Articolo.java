1deeea7d9f8ba680ad287699dd919530283d44cc
Carmen Caniglia <carmencaniglia@gmail.com>
2023-11-29 16:53:22 +0100

    inizio esercizio 3

---
+++ src/Es3/Articolo.java
@@ -0,0 +1,8 @@
+package Es3;
+
+public class Articolo {
+    private String codice;
+    private String descrizione;
+    private double prezzo;
+    private int numeroPezzi;
+}
---
+++ src/Es3/Cliente.java
@@ -0,0 +1,9 @@
+package Es3;
+
+public class Cliente {
+    private String codiceCliente;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String dataIscrizione;
+}
